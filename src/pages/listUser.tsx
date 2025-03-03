de0f7338488945715620e8bc847dc3cfaf7aecbd
InnovacionEam <innovacioneam@192.168.1.4>
2025-03-03 16:19:13 -0500

    se agrega y d

--- src/pages/listUser.tsx
+++ src/pages/listUser.tsx
@@ -1,7 +1,6 @@
 import { Table, Button, Space, Popconfirm, message, Modal, Input, Checkbox } from 'antd';
 import { EditOutlined, DeleteOutlined } from '@ant-design/icons';
-
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const ListUser: React.FC = () => {
 
@@ -11,28 +10,62 @@ const ListUser: React.FC = () => {
         state: string;
     }
 
-    var initialTasks: Task[] = [
-        { key: '1', task: 'task 1', state: "Pending" },
-        { key: '2', task: 'task 2', state: "Completed" },
-        { key: '3', task: 'task 3', state: "Pending" },
-    ];
-
     var instruccions = {
         "title": "Create a new task",
         "content": "Add a new task to the list.",
+        execute: () => handleOk()
     }
 
-    const [tasks, setTask] = useState<Task[]>(initialTasks);
-    const [newTask, setnewTask] = useState("");
+    const [tasks, setTasks] = useState<Task[]>([]);
+    const [newTask, setNewTask] = useState("");
     const [isModalOpen, setIsModalOpen] = useState(false);
     const [searchWord, setSearchWord] = useState("");
     const [modalData, setModalData] = useState(instruccions);
     const [checkBox, setCheckBox] = useState(Boolean);
-    const [completeList, setCompleteList] = useState<Task[]>(initialTasks);
+    const [selectedTask, setSelectedTask] = useState<Task | null>(null);
+    const [isCreating, setIsCreating] = useState(Boolean);
+    const [completeList, setCompleteList] = useState<Task[]>([]);
 
     const showModal = () => {
         setIsModalOpen(true);
     };
+
+    useEffect(() => {
+        if (isCreating) {
+            setModalData(
+                {
+                    "title": " Create a new task ",
+                    "content": `Add a task`,
+                    execute: () => {
+                        addTasks();
+                        setIsModalOpen(false);
+                        setNewTask("");
+                    }
+                }
+            )
+        } else {
+            setModalData(
+                {
+                    "title": "Edit task",
+                    "content": `Add a new field for the task: ${selectedTask?.task}`,
+                    execute: () => {
+                        setTasks(tasks.map(item =>
+                            item.key === selectedTask!.key ? { ...item, task: newTask } : item
+                        ));
+                        setNewTask("")
+                        setIsModalOpen(false);
+                    }
+                }
+            );
+        }
+
+    }, [newTask, isCreating])
+
+    useEffect( ()=> {
+        console.log(tasks);
+        
+        setCompleteList(tasks)
+    }, [tasks])
 
     const addTasks = () => {
         const nTask = {
@@ -40,76 +73,50 @@ const ListUser: React.FC = () => {
             task: newTask,
             state: "pending"
         };
-        setTask(prevTasks => [...prevTasks, nTask]);
-        setCompleteList(tasks)
+        setTasks(prevTasks => [...prevTasks, nTask]);
     };
 
     const handleOk = () => {
-
-        addTasks()
-        setIsModalOpen(false);
-        setnewTask("")
+        showModal()
+        setIsCreating(true)
     };
 
     const handleCancel = () => {
-
         setIsModalOpen(false);
     };
 
     const handleDelete = (key: string) => {
-        setTask(tasks.filter(task => task.key !== key));
+        setTasks(tasks.filter(task => task.key !== key));
         message.success('delete task');
     };
 
     const handleFilter = (state: string) => {
 
-        var completed = [];
-        var pending = [];
+        console.log(state);
+        
 
         if (state === "all") {
-            setTask(completeList)
+            setTasks(completeList)
         }
 
-        if (state === "Completed") {
-            completed = completeList.filter(task => task.state === state)
-            setTask(completed);
-        }
-
-        if (state === "Pending") {
-            pending = completeList.filter(task => task.state === state)
-            setTask(pending);
-        }
+        const filteredTasks = completeList.filter(task => task.state === state);
+        setTasks(filteredTasks);
+        
     };
 
     const handleEdit = (record: Task) => {
-
-        showModal()
-
-        const modalContnt = {
-            "title": "Edit task",
-            "content": `Add a new field for the task: ${record.task}`,
+        if (isModalOpen == false) {
+            showModal()
         }
 
-        tasks.forEach(task => {
-            if (task.key === record.key) {
-                task.task = record.task; // Modificamos directamente el array
-            }
-        });
-
-        setTask(tasks);
-        setModalData(modalContnt)
+        setIsCreating(false)
+        setSelectedTask(record);
     };
 
     const handleCheckBox = (record: Task, value: boolean) => {
-
-        initialTasks.forEach(task => {
-            if (task.key === record.key) {
-                task.state = value ? "Completed" : "Pending"; // Modificamos directamente el array
-                // setCheckBox( value ? true : false )
-            }
-        });
-        
-        setTask(initialTasks);
+        setTasks(tasks.map(item =>
+            item.key === record.key ? { ...item, state: value ? "completed" : "pending" } : item
+        ));
     };
 
     const columns = [
@@ -128,7 +135,7 @@ const ListUser: React.FC = () => {
             key: 'actions',
             render: (_: any, record: Task) => (
                 <Space>
-                    <Checkbox checked={record.state === "Completed"}  onChange={(e) => handleCheckBox(record, e.target.checked)}>
+                    <Checkbox checked={record.state === "completed"} onChange={(e) => handleCheckBox(record, e.target.checked)}>
                     </Checkbox>
                     <Button type="primary" icon={<EditOutlined />} onClick={() => handleEdit(record)}>
                         Edit
@@ -146,19 +153,17 @@ const ListUser: React.FC = () => {
 
             <Space direction="horizontal" size="large">
                 <h2>Tasks</h2>
-                <Button type="default" onClick={showModal}> Add a new Task </Button>
+                <Button type="default" onClick={handleOk}> Add a new Task </Button>
 
                 <Button type="default" onClick={() => handleFilter("all")}> All </Button>
-                <Button type="default" onClick={() => handleFilter("Completed")}> Completed </Button>
-                <Button type="default" onClick={() => handleFilter("Pending")}> Pending </Button>
+                <Button type="default" onClick={() => handleFilter("completed")}> Completed </Button>
+                <Button type="default" onClick={() => handleFilter("pending")}> Pending </Button>
             </Space>
-
-
 
             <Modal
                 title={modalData.title}
                 open={isModalOpen}
-                onOk={handleOk}
+                onOk={modalData.execute}
                 onCancel={handleCancel}
                 okText="Confirm"
                 cancelText="Cancel"
@@ -168,8 +173,8 @@ const ListUser: React.FC = () => {
                 <div style={{ display: 'flex', justifyContent: 'center' }}>
                     <Input
                         placeholder="Write something."
-                        value={newTask}
-                        onChange={(e) => setnewTask(e.target.value)}
+                        value={ newTask }
+                        onChange={(e) => setNewTask(e.target.value)}
                         allowClear
                         style={{ width: 300 }}
                     />
